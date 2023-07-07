5e31525c0017667a839ccbf19826375169333d53
Everett Summer <55684557+EverettSummer@users.noreply.github.com>
2023-07-07 08:15:30 -0700

    add a backup ip getter

--- src/IPv4Finder.ts
+++ src/IPv4Finder.ts
@@ -6,7 +6,7 @@ import axios, { Axios } from 'axios';
 const sleep = promisify(setTimeout) as (time: number) => void;
 
 const logger = getStdLogger();
-const PUBLIC_IP_GETTER = 'https://api.myip.la';
+const PUBLIC_IP_GETTER = ['https://api.myip.la', 'https://api.ipify.org'];
 const MAX_ERROR_COUNT = 20;
 
 const RETRY_INTERVAL = 30000;
@@ -14,14 +14,17 @@ const RETRY_INTERVAL = 30000;
 export class IPv4Finder {
     private errorCount = 0;
     private axiosInstance: Axios;
+    private _ipGetter: string;
+    private _ipGetterIdx = 0;
 
     constructor() {
         this.axiosInstance = axios.create({});
+        this._ipGetter = PUBLIC_IP_GETTER[this._ipGetterIdx];
     }
 
     public async getMyIp(): Promise<string> {
         try {
-            const resp = await this.axiosInstance.get(PUBLIC_IP_GETTER, {responseType: 'text'});
+            const resp = await this.axiosInstance.get(this._ipGetter, {responseType: 'text'});
             this.errorCount = 0;
             return resp.data as string;
         } catch (ex) {
@@ -32,6 +35,7 @@ export class IPv4Finder {
                 captureException(new Error('getMyIP max error count reached'));
                 return null;
             }
+            this._ipGetter = PUBLIC_IP_GETTER[Math.abs(this._ipGetterIdx - 1)];
             await sleep(RETRY_INTERVAL);
             return await this.getMyIp();
         }
